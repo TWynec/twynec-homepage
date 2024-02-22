a80e4602b83320b606826d127ee6e4359645d936
Trace Wynecoop <trwyne03@gmail.com>
2024-02-21 21:43:27 -0800

    Add contact and bio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Heading,
+  List,
+  ListItem,
+  Link,
+  Button,
+} from "@chakra-ui/react";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 import Paragraph from "../components/paragraph";
 import Section from "../components/section";
+import NextLink from "next/link";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 const Page = () => {
   return (
@@ -30,6 +41,36 @@ const Page = () => {
           forefront of innovation and have a track record of developing products
           that have reached tens of millions of users.
         </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/work">
+            <Button rightIcon={<ChevronRightIcon />}>Portfolio</Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Contact
+        </Heading>
+        <List>
+          <ListItem>
+            <Link
+              href="https://www.linkedin.com/in/tracewynecoop/"
+              target="_blank"
+            >
+              <Button variant="ghost" leftIcon={<IoLogoLinkedin />}>
+                Trace Wynecoop
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://github.com/TWynec" target="_blank">
+              <Button variant="ghost" leftIcon={<IoLogoGithub />}>
+                @TWynec
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
       </Section>
     </Container>
   );
